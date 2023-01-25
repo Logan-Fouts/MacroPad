e9e0348140bfcdb313ec42d3b0ba54238271c252
Logan Fouts <112691772+Logan-Fouts@users.noreply.github.com>
2023-01-25 16:30:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from tkinter import *
+from tkinter import ttk
 import tkinter as tk
+from ttkthemes import ThemedTk
 import webbrowser
 
 computer = "lfouts"
@@ -34,31 +37,37 @@ def open_browser():
 
 read()
 
-window = tk.Tk()
+window = ThemedTk(theme="default")
+window.geometry("800x400")
 window.title("Macro Pad Gui")
+
+style = ttk.Style()
+style.configure("My.TEntry", relief="sunken", borderwidth=2)
+style.configure("My.Label", font=("TkDefaultFont", 10, "bold"))
 
 def handlemode():
     global mode
     global entries
     if mode == 0:
         print(mode)
-        label = tk.Label(text="Profile: 0")
+        label = ttk.Label(text="Profile: 0", style="My.Label")
         label.grid(row=0, column=3)
         mode = 1
         k = 0
         entries = []
         for i in range(1, 4):
+            if i == 3:
+                window.columnconfigure(4, weight=1, minsize=75)
+                window.rowconfigure(4, weight=1, minsize=50)
             window.columnconfigure(i, weight=1, minsize=75)
             window.rowconfigure(i, weight=1, minsize=50)
 
             for j in range(1, 5):
-                frame = tk.Frame(
-                    master=window,
-                    relief=tk.RAISED,
-                    borderwidth=1
+                frame = ttk.Frame(
+                    master=window
                 )
                 frame.grid(row=i, column=j, padx=5, pady=5)
-                entry = tk.Entry(master=frame, width=50)
+                entry = ttk.Entry(master=frame, width=50, style="My.TEntry")
                 entry.insert(0, content1[3+k][4:100])
                 entry.pack(padx=5, pady=5)
                 entries.append(entry)
@@ -68,37 +77,41 @@ def handlemode():
         print(mode)
         mode = 0
         entries = []
-        label = tk.Label(text="Profile: 1")
+        label = ttk.Label(text="Profile: 1", style="My.Label")
         label.grid(row=0, column=3)
         for i in range(1, 4):
+            if i == 3:
+                window.columnconfigure(4, weight=1, minsize=75)
+                window.rowconfigure(4, weight=1, minsize=50)
             window.columnconfigure(i, weight=1, minsize=75)
             window.rowconfigure(i, weight=1, minsize=50)
 
             for j in range(1, 5):
                 frame = tk.Frame(
-                    master=window,
-                    relief=tk.RAISED,
-                    borderwidth=1
+                    master=window
                 )
                 frame.grid(row=i, column=j, padx=5, pady=5)
-                entry = tk.Entry(master=frame, width=50)
+                entry = ttk.Entry(master=frame, width=50, style="My.TEntry")
                 entry.insert(0, content2[3+k][4:100])
                 entry.pack(padx=5, pady=5)
                 entries.append(entry)
                 k += 1
-                
+          
+style = ttk.Style()
+style.configure("My.TLabel", font=("TkDefaultFont", 20, "bold"))
 
-title1 = tk.Label(text="Key Binds")
+      
+title1 = ttk.Label(text="Key Binds", style="My.TLabel")
 title1.grid(row=0, column=2)
 
-button = tk.Button(window, text="Open Help", command=open_browser)
-button.grid(row=2, column=5)
+button = ttk.Button(window, text="Open Help", command=open_browser, style="My.TButton")
+button.grid(row=0, column=4)
 
-button2 = tk.Button(text="Save", command=lambda: handleclick(entries))
-button2.grid(row=0, column=5)
+button2 = ttk.Button(text="Save", command=lambda: handleclick(entries), style="My.TButton")
+button2.grid(row=4, column=3)
 
-button3 = tk.Button(text="Switch Mode", command=lambda: handlemode())
-button3.grid(row=1, column=5)
+button3 = ttk.Button(text="Switch Mode", command=lambda: handlemode(), style="My.TButton")
+button3.grid(row=4, column=2)
 
 handlemode()
 
