3a1ec4a8ae8539b29c00f3fef8349986cb1f7d98
Logan Fouts <112691772+Logan-Fouts@users.noreply.github.com>
2023-01-25 16:30:00 +0100

    Update Mode1.py

--- Mode1.py
+++ Mode1.py
@@ -1,14 +1,14 @@
 from adafruit_hid.keycode import Keycode
 def getKeys0():
     keymap = {
-    (0): ((Keycode.LEFT_ALT, Keycode.F6)),
-    (1): (Keycode.LEFT_ALT, Keycode.F5),
-    (2): (Keycode.GUI, Keycode.TAB),
-    (3): (Keycode.LEFT_ALT, Keycode.F12),
-    (4): ([Keycode.KEYPAD_FIVE]),
-    (5): ([Keycode.KEYPAD_TWO]),
-    (6): (Keycode.LEFT_ALT, Keycode.G),
-    (7): (Keycode.LEFT_ALT, Keycode.F4),
+    (0): (Keycode.LEFT_ALT, Keycode.F6),
+    (1): (Keycode.LEFT_ALT, Keycode.F12),
+    (2): (Keycode.LEFT_ALT, Keycode.G),
+    (3): (Keycode.LEFT_CONTROL, Keycode.K),
+    (4): (Keycode.LEFT_ALT, Keycode.F5),
+    (5): ([Keycode.A]),
+    (6): (Keycode.LEFT_ALT, Keycode.F4),
+    (7): ([Keycode.O]),
     (8): ([Keycode.LEFT_ALT, Keycode.F2]),
     (9): (Keycode.LEFT_CONTROL, Keycode.K),
     (10): (Keycode.LEFT_CONTROL, Keycode.D),
